58c2858d8e4ac34cc563d79a89099a9e44cc76b8
MiguelCedano95 <migueced95@gmail.com>
2025-11-03 18:50:20 -0700

    Update Conecta4.cpp

--- Conecta4.cpp
+++ Conecta4.cpp
@@ -1,23 +1,8 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <cctype>
+#include <string>
 using namespace std;
 
-const int ROWS = 6;
-const int COLS = 7;
-const char EMPTY = ".";
-const char J1 = "X";
-const char J2 = "O";
+const int FILAS = 6;
+const int COLUMNAS = 7;
 
-using Board = vector<vector<char>>;
-
-Board createBoard() {
-    return Board(ROWS, vector<char>(COLS, EMPTY));
-}
-
-void printBoard(const Board &b) {
-    cout << "";
-    for (int c = 1; c <= COLS; ++c) cout << c << ' ';
-    cout << " ";
-}
